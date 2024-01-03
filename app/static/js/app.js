fc4601e0ca38acdb0290fa60db0dc16af0b85408
Truc Thanh <trucnguyenthanh56@gmail.com>
2024-01-03 22:26:02 +0700

    finish form book tickets

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -9,3 +9,24 @@ $(".slider-content").slick({
 	nextArrow:
 		"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
 });
+
+function quantityTickets(type){
+    let quantityInput = document.getElementById('quantity-tickets');
+    let currentQuantity = parseInt(quantityInput.value);
+    if (type === 'increase') {
+        quantityInput.value = currentQuantity + 1;
+    } else if ( type === 'decrease' && currentQuantity > 1) {
+      quantityInput.value = currentQuantity - 1;
+    }
+}
+
+function toggleActiveModal(clickedBtn){
+    let btns = document.querySelectorAll('.modal-header-btn .nav-item')
+    btns.forEach((btn)=>{
+        btn.classList.remove('active-modal-header')
+    })
+    clickedBtn.classList.add('active-modal-header')
+
+    let roundTrip = document.querySelector('.modal-header-btn .nav-item.round-trip')
+    document.querySelector('#return-date').disabled = roundTrip.classList.contains('active-modal-header') ? true : false
+}
